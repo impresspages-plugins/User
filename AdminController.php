e7a0cf313165cda7f39a0bda27d39b4cbb7082b6
Mangirdas <mangirdas@impresspages.org>
2014-05-17 21:09:30 +0300

    user edit

--- AdminController.php
+++ AdminController.php
@@ -13,13 +13,12 @@ class AdminController extends \Ip\GridController
             'sortField' => 'id',
             'createPosition' => 'top',
             'allowCreate' => false,
-            'allowUpdate' => false,
-            'allowDelete' => false,
             'allowSort' => false,
             'fields' => array(
                 array(
                     'label' => __('Id', 'User', false),
                     'field' => 'id',
+                    'allowUpdate' => false
                 ),
                 array(
                     'label' => __('User name', 'User', false),
@@ -28,11 +27,20 @@ class AdminController extends \Ip\GridController
                     'label' => __('Email', 'User', false),
                     'field' => 'email',
                 ),
-            )
+                array(
+                    'label' => __('Password', 'User', false),
+                    'field' => 'hash',
+                    'type' => 'Password'
+                ),
+            ),
+            'beforeDelete' => array('Plugin\User\GridHelper', 'beforeDelete'),
+            'beforeUpdate' => array('Plugin\User\GridHelper', 'beforeUpdate'),
+            'updateFilter' => array('Plugin\User\GridHelper', 'updateFilter')
         );
 
         $gridConfig = ipFilter('User_adminGridConfig', $gridConfig);
         return $gridConfig;
     }
+
 
 }
---
+++ GridHelper.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package   ImpressPages
+ */
+
+
+
+
+namespace Plugin\User;
+
+
+class GridHelper
+{
+    public static function beforeDelete($id)
+    {
+        Service::delete($id);
+    }
+    public static function beforeUpdate($id, $postData)
+    {
+        Service::update($id, $postData);
+    }
+
+    public static function updateFilter($id, $data)
+    {
+        //remove username and email as these fields will be updated by beforeUpdate.
+        unset($data['username']);
+        unset($data['email']);
+        return $data;
+    }
+}
