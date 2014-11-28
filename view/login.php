2e3363093b93626e098ae38e1da8590baf5289ab
Mangirdas Skripka <mangirdas@impresspages.org>
2014-11-28 22:25:04 +0200

    update login view

--- view/login.php
+++ view/login.php
@@ -1,5 +1,13 @@
+
+
 <?php echo ipRenderWidget('Heading', array('title' => __('Login', 'User', false))) ?>
-<?php echo $form->render() ?>
-<a class="ipUserRegistrationLink" href="<?php echo escAttr($registrationUrl) ?>"><?php _e('Register', 'User') ?></a>
-</br>
-<a class="ipUserPasswordResetLink" href="<?php echo escAttr($passwordResetUrl) ?>"><?php _e('Forgot password', 'User') ?></a>
+<div class="ipWidget">
+    <?php echo $form->render() ?>
+</div>
+<div class="ipWidget">
+    <p>
+        <a class="ipUserRegistrationLink" href="<?php echo escAttr($registrationUrl) ?>"><?php _e('Register', 'User') ?></a>
+        </br>
+        <a class="ipUserPasswordResetLink" href="<?php echo escAttr($passwordResetUrl) ?>"><?php _e('Forgot password', 'User') ?></a>
+    </p>
+</div>
